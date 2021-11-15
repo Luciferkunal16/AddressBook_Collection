29724b0d23c37fd039dd5180c8e34b786c67d300
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-15 18:24:24 +0530

    Added UC7: NO duplicate Person in AddressBook

---
+++ src/AddressBookMain.java
@@ -0,0 +1,16 @@
+package AddressBook;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static Scanner scannerObject = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.println("---------- Welcome To Address Book Pr1ogram!! ----------");
+		AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+		addressBookDirectory.operationDirectory();
+	}
+
+}
